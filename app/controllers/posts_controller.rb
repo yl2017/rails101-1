1a24d428e376ce7903db5ab3420578d10a6fc521
yl2017 <yl15327117612@gmail.com>
2017-03-27 15:59:05 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def new
     @group = Group.find(params[:group_id])
-    @group = Post.new
+    @post = Post.new
   end
 
   def create
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.requier(:post).permit(:content)
+    params.require(:post).permit(:content)
   end
 
 end
